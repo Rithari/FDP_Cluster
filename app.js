71aca03bd905ba31c27ded42955cee43b3cd241f
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2024-01-21 23:28:25 +0100

    Adds player API routes, table caching

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const chat = require("./routes/chat/chat");
+const players = require("./routes/players/players");
+const competitions = require("./routes/competitions/competitions");
+const clubs = require("./routes/clubs/clubs");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+const server = http.createServer(app);
+const io = socketIo(server);
 
-module.exports = app;
+chat(io); // Pass the io instance to the chat module
+
+app.use(cors());
+app.use("/api", players);
+app.use("/api", competitions);
+app.use("/api", clubs);
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
