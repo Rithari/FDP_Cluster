33908fd0c8f9718dcafc8fd35501b0313245fe6a
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2024-01-22 21:18:11 +0100

    Defines cors and socketIO server

--- app.js
+++ app.js
@@ -10,12 +10,23 @@ const clubs = require("./routes/clubs/clubs");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"], // Allowed HTTP methods
+};
+
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 chat(io); // Pass the io instance to the chat module
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/api", players);
 app.use("/api", competitions);
 app.use("/api", clubs);
