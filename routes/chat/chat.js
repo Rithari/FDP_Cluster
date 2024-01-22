d46faeffa7ec82c2833c79511f4afaf606648457
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2024-01-22 21:18:32 +0100

    Rewrites chat backend

--- routes/chat/chat.js
+++ routes/chat/chat.js
@@ -1,24 +1,32 @@
 module.exports = function (io) {
-    io.on('connection', (socket) => {
-        console.log('A user connected');
+  let users = [];
 
-        // Main server room
-        socket.join('main');
-
-        socket.on('chat message', (data) => {
-            const timestamp = new Date().toISOString();
-            console.log(data);
-            const msg = data;
-            const user = data.user || `Anonymous${Math.floor(Math.random() * 1000)}`;
-            console.log(`[${timestamp}] Message from ${user}: ${msg}`);
-            io.to('main').emit('chat message', { user, msg, timestamp });
-        });
-
-        socket.on('disconnect', () => {
-            console.log('User disconnected');
-        });
+  io.on("connection", (socket) => {
+    socket.on("message", (data) => {
+      io.emit("messageResponse", data);
     });
 
-    
-    
-};  
+    socket.on("typing", (data) =>
+      socket.broadcast.emit("typingResponse", data)
+    );
+
+    socket.on("newUser", (data) => {
+      // Check if the username is provided, otherwise assign an anonymous name
+      if (!data.username) {
+        data.username = `Anonymous${Math.floor(Math.random() * 1000)}`;
+      }
+
+      // Include the socket ID in the user's data
+      data.socketID = socket.id;
+      users.push(data);
+
+      io.emit("newUserResponse", users);
+    });
+
+    socket.on("disconnect", () => {
+      users = users.filter((user) => user.socketID !== socket.id);
+      io.emit("newUserResponse", users);
+      socket.disconnect();
+    });
+  });
+};
