c4ba896950022c905244d01542a7710e2ceafbdd
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2024-01-23 01:25:13 +0100

    Adds competition clubs servicing

--- routes/clubs/clubs.js
+++ routes/clubs/clubs.js
@@ -42,4 +42,25 @@ router.get("/clubs/:clubId", async (req, res) => {
   }
 });
 
+router.get("/clubs/competition/:competitionId", async (req, res) => {
+  const competitionId = req.params.competitionId;
+  const cacheKey = `clubsByCompetition_${competitionId}`;
+  const cachedData = clubsCache.get(cacheKey);
+
+  if (cachedData) {
+    return res.json(cachedData);
+  }
+
+  try {
+    const response = await axios.get(
+      `http://localhost:8080/clubs/competition/${competitionId}`
+    );
+    clubsCache.set(cacheKey, response.data);
+    res.json(response.data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
 module.exports = router;
